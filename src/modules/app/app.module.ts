049aff3daad939c81cbcdcb99f9b5429c190aa4a
Yurii Horchuk <fkca.av15gyua@gmail.com>
2023-03-24 16:12:13 +0200

    init commit

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AppController } from '../../presentation/controllers/app.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+})
+export class AppModule {}
